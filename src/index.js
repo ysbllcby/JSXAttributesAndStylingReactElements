8d0221eaf0a726de53ceeae713a654cc2c168d88
Ma Cubay <ma.cubay@edu.sait.ca>
2023-05-27 21:17:03 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <img
+      className="food-img"
+      alt="waffers"
+      src="https://cdn.shopify.com/s/files/1/0276/0435/3122/products/image_87a7a145-70b6-415d-ab44-21bc3a29e650.png?v=1635362724"
+    />
+    <img
+      className="food-img"
+      alt="pizza"
+      src="https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2021-07-Chicken-Alfredo-Pizza%2FChicken-Alfredo-Pizza-KitchnKitchn2970-1_01"
+    />
+    <img
+      className="food-img"
+      alt="icecream"
+      src="https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSRA1VvVWshDqdk-IbClyNwWoUAjMnFQ5KQLb8eH0hu4gCCvfvqMEcQqOF-CTro2maW"
+    />
+  </div>,
+  document.getElementById("root")
+);
